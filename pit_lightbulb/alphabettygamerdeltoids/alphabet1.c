949798067e7563629d0f5ebc0ecc5cf51a638f3d
hello-magikarp <s00030124@epicode.edu.mt>
2026-01-19 17:25:12 +0100

    Add files via upload

---
+++ pit_lightbulb/alphabettygamerdeltoids/alphabet1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// LEVEL 1 of the alphabet exercise
+// : var declaration and for loop
+
+int main() {
+
+    char c= 'A';
+    for(char c = 'A'; c <= 'Z'; c++)
+    {
+        printf("%c\n", c);
+    }
+    
+    
+}
